05bcc5c8ac7e64c862cb7709d640c20c232fdc29
Patricia Loto <39300655+PatriLoto@users.noreply.github.com>
2019-07-01 03:58:27 -0300

    ultima version

--- Rladies.R
+++ Rladies.R
@@ -9,6 +9,7 @@ library(ggthemes)
 library(wesanderson)
 library(DT)
 library(maps)
+library(plotly)
 # install_packages("readr")
 capitulos_rladies <- readr::read_csv("https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-06-26/capitulos_rladies.csv")
 eventos_rladies <- readr::read_csv("https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-06-26/eventos_rladies.csv")
@@ -170,17 +171,17 @@ ggsave("mapaRLadies5.png",width = 10, height = 5, dpi = "retina")
 #5ta version
 #-----------------------------------------------------------
 map7 <- world2 +
-  geom_point(data = capitulos_rladies, aes(x = longitud, y = latitud,
+  geom_point(aes(x = longitud, y = latitud,
                  text = paste('city: ', ciudad,
                               '<br /> created : ', creacion),
-                 size = miembros, alpha = miembros, colour = "#88398A")) +     #562457#88398A  #alpha = .5
+                 size = miembros), data = capitulos_rladies, colour = "#88398A", alpha = .5)  +     #562457#88398A  #alpha = .5
   scale_size_continuous(range = c(1, 10), breaks = c(250, 500,1000,1500,2000)) +
  # scale_color_continuous(option="purple", trans="log", breaks=c(250, 500, 750, 1000,1250,1500,1750,2000)) +
-  scale_alpha_continuous(trans="log") +
-  #theme_void() +
-  #ylim(50,59) +
+  #scale_alpha_continuous(trans="log") +
+  theme_void() +
+ # ylim(50,59) +
   #coord_map() +
-  labs(size = 'Miembros Meetups') +
+  labs(size = '') +
   theme(legend.position = "left",                ###c(0.85, 0.8),
     legend.text = element_text(colour ="#446455" , size = 8),
     legend.title = element_text(colour = "#446455", size = 10),   #actor_colour="#446455"
@@ -188,18 +189,42 @@ map7 <- world2 +
     legend.background = element_rect(fill = "#D3DDDC", colour =NA),  #colour = #446455=, "#miembros"
     panel.background = element_rect(fill = "#D3DDDC", colour =NA),
     plot.background = element_rect(fill = "#D3DDDC", colour = "#D3DDDC"),
-    plot.title = element_text(colour = wes_palette("GrandBudapest1")[2], size = 22, hjust = 0.5, family = "FuturaBT-ExtraBlack", face="bold"),
-    plot.subtitle = element_text(colour = "#446455", size = 16, hjust = 0.5,family = "FuturaBT-ExtraBlack", face="italic"),
+    plot.title = element_text(colour = wes_palette("GrandBudapest1")[2], size = 22, hjust = 0.5, family = "FuturaBT-ExtraBlack", face="bold"),#wes_palette("GrandBudapest1")[2]
+    plot.subtitle = element_text(colour = "#446455", size = 14, hjust = 0.5,family = "FuturaBT-ExtraBlack", face="italic"),
     plot.caption = element_text(colour =  wes_palette("GrandBudapest1")[2], size = 10, hjust = 0.5,face="bold", vjust=1))+
     #plot.margin = margin(0.5, 0.1, 0.5, 0.1, "cm")) +
   labs(title = toupper("Rladies en el mundo"),
-       subtitle = "Cantidad de miembros en Meetups hasta el 25 de junio del 2019",
-       caption = "@patriloto | Source:#DatosdeMiercoles")
+       subtitle = "Cantidad de miembros por capítulo hasta el 25 de junio del 2019",
+       caption = "#DatosDeMiercoles por Patricia Loto")
 
 map7
+plotly (map7)
+
+
+
 
 
 #transition_time(importaXP) +
 #  ease_aes('linear')+
  #  shadow_mark(alpha = 1, size = 2)
-ggsave("mapaRLadies8.png",width = 10, height = 5, dpi = "retina")
+ggsave("mapaRLadies10.png",width = 10, height = 5, dpi = "retina")
+
+#-------------------------------------
+#animacion
+#------------------------------------
+library(gganimate)
+map7+
+transition_time(miembros) +
+  ease_aes('linear')+
+  shadow_mark(alpha = 1, size = 2)
+
+gganimate(map7, interval = .2, filename = 'rladies.gif')
+#-------------------------------------------------------
+map7 <- world2 +
+  geom_point(data = capitulos_rladies, aes(x = longitud, y = latitud,
+                                           text = paste('city: ', ciudad,
+                                                        '<br /> created : ', creacion),
+                                           size = miembros, alpha = miembros, colour = "#88398A")) +     #562457#88398A  #alpha = .5
+  scale_size_continuous(range = c(1, 10), breaks = c(250, 500,1000,1500,2000)) +
+  # scale_color_continuous(option="purple", trans="log", breaks=c(250, 500, 750, 1000,1250,1500,1750,2000)) +
+  scale_alpha_continuous(trans="log") +
