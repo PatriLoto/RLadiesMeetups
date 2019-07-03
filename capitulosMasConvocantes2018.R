305ada6bbd5200229f71eab9624ba95fb0464a35
Patricia Loto <lotcha.aloto@chaco.gov.ar>
2019-07-03 08:30:32 -0300

    visualizaciones de capitulos más convocantes con dos Berry y Tangerine

--- capitulosMasConvocantes2018.R
+++ capitulosMasConvocantes2018.R
@@ -1,21 +1,14 @@
 extrafont::loadfonts(device = "win")
 library(tidyverse)
-#library(ggridges)
 library(paletteer)
 library("LaCroixColoR")
-#library(ggbeeswarm)
 library(lubridate)
-#library(fonts)
 library(LaCroixColoR)
-#library(cowplot)
-#library(viridis)
 library(png)
 library(grid)
-install.packages("ggbeeswarm")
-install.packages("ggridges")
 install.packages("fonts")
 install.packages("themes")
-install.packages("cowplot")
+
 eventos_rladies <- readr::read_csv("https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-06-26/eventos_rladies.csv")
 
 datatable(eventos_rladies, rownames = FALSE,
@@ -24,6 +17,10 @@ datatable(eventos_rladies, rownames = FALSE,
 #total de asistentes desde su creación al 25 de junio del 2019
 asistentesXCap <-eventos_rladies %>% group_by(capitulo)%>% summarise(totalasist=sum(respuesta_asistire))%>% arrange(desc(totalasist))
 View(asistentesXCap)
+
+#total de asistentes ordenado por año y cantidad de asistentes desde su creación al 25 de junio del 2019
+asistentesXYearCantidad<-eventos_rladies %>% group_by(capitulo, anio=year(fecha_local))%>% summarise(totalasist=sum(respuesta_asistire))%>% arrange(desc(anio,totalasist))
+View(asistentesXYearCantidad)
 
 #total de asistentes ordenado por año y capitulo desde su creación al 25 de junio del 2019
 asistentesXYearCap <-eventos_rladies %>% group_by(capitulo, anio=year(fecha_local))%>% summarise(totalasist=sum(respuesta_asistire))%>% arrange(anio,totalasist)
@@ -34,21 +31,21 @@ asistentesXYearCap[asistentesXYearCap$capitulo == "Spotkania Entuzjastów R-Warsa
 asistentes2018XCap <-filter(asistentesXYearCap, anio==2018)%>%arrange(desc(totalasist))
 asistentes2018XCap
 View(asistentes2018XCap)
+
 #renombro el capitulo de Warsaw
 asistentes2018XCap[asistentes2018XCap$capitulo == "Spotkania Entuzjastów R-Warsaw RUG Meetup & R-Ladies Warsaw",1]<-"R-Ladies Warsaw"
 
 top20convocantes <-asistentes2018XCap %>%arrange(desc(totalasist)) %>% head(20)                 #top_n(-10)
 View(top20convocantes)
 
-#total de asistentes ordenado por año y cantidad de asistentes desde su creación al 25 de junio del 2019
-asistentesXYearCantidad<-eventos_rladies %>% group_by(capitulo, anio=year(fecha_local))%>% summarise(totalasist=sum(respuesta_asistire))%>% arrange(desc(anio,totalasist))
-View(asistentesXYearCantidad)
-
-
 #grafico twitter
+# con paleta lacroix_palette - Berry
+pal_1 <- lacroix_palette("Berry", n = 20, type = "continuous")
+pal_1
+
 p20 <-ggplot(top20convocantes, aes(reorder(capitulo, totalasist), totalasist, fill=(capitulo))) +
   geom_col(alpha=0.8) +
-  scale_fill_manual(values =  pal_2) +
+  scale_fill_manual(values =  pal_1) +
   #geom_text(size = 3, hjust = 1.5,colour = "black")+
   coord_flip() + 
   theme_light() +
@@ -67,13 +64,12 @@ p20 <-ggplot(top20convocantes, aes(reorder(capitulo, totalasist), totalasist, fi
        caption =
          "Data: #DatosDeMiercoles \n por Patricia Loto")
 p20
+
 # inserto logo
 img <- readPNG("D:/Patri/RLADIES-RCIA-CTES/LOGOS_FLYER/rladies.PNG", FALSE)
 marca <- rasterGrob(img, interpolate=F,height=unit(3, "cm"),hjust=-2.2, vjust=1.5)
 pmarca <-p20 + annotation_custom(marca,xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
 ggsave("blueberry6.png",width = 10, height = 5, dpi = "retina")
-
-
 
 # en breve terminaré la animacion
 library(gganimate)
@@ -83,12 +79,11 @@ pmarca+
   shadow_mark(alpha = 1, size = 2)
 
 
-# con paleta lacroix_palette
+# con paleta lacroix_palette - Tangerine
 pal_2 <- lacroix_palette("Tangerine", n = 20, type = "continuous")
 
-ggplot(top20convocantes, aes(reorder(totalasist), x =(capitulo), y = totalasist, fill=(capitulo))) +
+ggplot(top20convocantes, aes(reorder(capitulo, totalasist), totalasist, fill=(capitulo))) +
   geom_col(alpha=0.8) +
-  #scale_color_manual(values = pal_2)+
   scale_fill_manual(values =  pal_2) +
   #scale_fill_viridis_c(option = "B") +
   coord_flip() + 
@@ -108,31 +103,15 @@ ggplot(top20convocantes, aes(reorder(totalasist), x =(capitulo), y = totalasist,
        subtitle = "Dos son de latinoamérica",
        caption =
          "Data: #DatosDeMiercoles \n por Patricia Loto")
-ggsave("Tangerine.png",width = 10, height = 5, dpi = "retina")
-# con paleta lacroix_palette
-pal_2 <- lacroix_palette("Berry", n = 20, type = "continuous")
-pal_2
-#reorder(nombreProducto, importaXP), importaXP
 
-
-
-
-
-
+ ggsave("Tangerine.png",width = 10, height = 5, dpi = "retina")
 
 
 p <-asistentesXYearCap%>%arrange(anio,totalasist)%>%
 ggplot(aes(x = reorder(anio, totalasist), fill = capitulo))+
   geom_col(show.legend = TRUE) + 
   scale_size(range = c(2, 1500))+
-  geom_text(
-    aes(label = totalasist),
-    #data = lealtades,
-    color = "grey",
-    angle=90,
-    size=3,
-    alpha = 0.80)+
-  #scale_size_continuous(range = c(1, 8), breaks = c(250, 500, 750, 1000,1250,1500,1750,2000)) +
+ #scale_size_continuous(range = c(1, 8), breaks = c(250, 500, 750, 1000,1250,1500,1750,2000)) +
   scale_color_manual(values = pal_1) +
   scale_fill_manual(values = pal_1) +
  coord_flip() + 
@@ -150,13 +129,10 @@ ggplot(aes(x = reorder(anio, totalasist), fill = capitulo))+
         plot.title = element_text(colour = "#562457", size = 22, hjust = 0.5, family = "Palatino Linotype",face="bold"),
         plot.subtitle = element_text(colour = "grey", size = 16, hjust = 0.5),
         plot.caption = element_text(colour =  wes_palette("Darjeeling2")[2], size = 8, hjust = 0.5, face="bold"),
-       # plot.subtitle = element_text(color = "black", hjust = 0.5),
-       # plot.caption = element_text(color = "black"),
-        #plot.title = element_text(size = 18, hjust = 0.5),
         axis.title = element_text(size = 14),
         axis.text = element_text(size = 12)
   )
-ggsave("assitentesAlAnio.png",width = 10, height = 5, dpi = "retina")
+ggsave("asistentesAlAnio.png",width = 10, height = 5, dpi = "retina")
 p
 
 
